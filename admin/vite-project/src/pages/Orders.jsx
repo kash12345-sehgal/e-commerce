b35193ce9a914c3c415c615f8a079a4816e5c86c
kash12345-sehgal <kashishsehgal1111@gmail.com>
2026-04-27 08:29:30 +0530

    orders

--- admin/vite-project/src/pages/Orders.jsx
+++ admin/vite-project/src/pages/Orders.jsx
@@ -1,57 +1,37 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
+
+
 
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
-import {backendUrl} from '../App'
-import toast from 'react_toastify'
-const Orders = ({token}) => {
-  const [orders, setOrders] = useState([]);
+import { backendUrl, currency } from '../App'
+import { toast } from 'react-toastify'
 
-  const fetchAllOrders = async () => {
-  
-    if (!token) {
-      return null
-    }
-    try {
-      const response = await axios.post(backendUrl + '/api/order/list', {}, { headers: { token }})
-      if (response.data.success) {
-        setOrders(response.data.orders)
-       
-      } else {
-        toast.error(response.data.message)
+
+const Orders = ({token}) => {
+  const [orders,setOrders]= useState([])
+    const fetchAllOrders = async () => {
+      if(!token){
+        return null;
+      }
+      try{
+        const response = await axios.post(backendUrl+'/api/order/list',{},{header:{token}})
+console.log(response.data)
+      }catch(error){
 
       }
-
-    } catch (error) {
-toast.error(error.message)
+      
     }
-  }
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    fetchAllOrders();
-  }, [token])
-
+    useEffect(()=>{
+fetchAllOrders()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[token])
+  
   return (
     <div>
-<h3>Order Page</h3>
-<div>
-  {
-    orders.map((order,index) => (
-      <div key={index}>
-        <img src={assets.parcel_icon} alt=''/>
-        <div>
-          {orders.items.map(()=>{
-            
-          })}
-          </div>
-      </div>
-    ))
-}
-</div>
+
     </div>
   )
 }
